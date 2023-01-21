66d393345d2a82749a2927f301ccf65ce69cd637
Shizz74 <wojciechbajko@gmail.com>
2023-01-21 12:29:31 +0100

    Init component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyTableComponent } from './currency-table/currency-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyTableComponent
   ],
   imports: [
     BrowserModule,
